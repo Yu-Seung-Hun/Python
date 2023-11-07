65dff0b1ae71c4124056cc15df43ea2bb5ef3ffa
Yu-Seung-Hun <youu0208@naver.com>
2023-11-07 20:46:12 +0900

    SWEA update

--- SWEA/1859.py
+++ SWEA/1859.py
@@ -1,22 +1,6 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-
-# data = list(map(int, input().split())) 공백으로 구분하여 여러개 데이터 입력받은 후 리스트로 저장.
-# sys 라이브러리에 정의되어 잇는 sys.stdin.readline() 함수를 이용하면 한줄씩 입력받을 수 있다.
-# sys.stdin.readline()로 한줄을 입력받은 후에는 rstrip() 함수를 꼭 호출해야한다.
-# 엔터가 줄 바꿈 기호로 입력되는데, 이 공백 문자를 제거하려면 rstrip() 함수를 사용해야 한다.
-
-# from itertools import permutations 순열
-# from itertools import combinations 조합
-# from itertools import product 중복허용 뽑기
-# from itertools import combinations_with_replacement 중복허용 조합
-# import heapq
-# from bisect import bisect_left, bisect_right
-# from collections import deque
-# from collections import Counter
-
-
  
 T = int(input()) # 전체 TC 갯수
 
